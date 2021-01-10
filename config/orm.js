d2a83ffb4b152aeb26fe99ec0576c5f318e23495
JC <sanjothebay@gmail.com>
2021-01-09 23:27:14 -0600

    added my function as read me

---
+++ config/orm.js
@@ -0,0 +1,15 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+
+function selectAll() {
+    
+};
+
+function insertOne() {
+    
+};
+
+function updateOne() {
+    
+};
