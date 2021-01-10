25c4919df364b2f2fe5727367658c564f9805d92
JC <sanjothebay@gmail.com>
2021-01-09 23:23:08 -0600

    ok

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (burger_name) VALUE ("Double Double Burger");
-INSERT INTO burgers (burger_name) VALUE ("4 X Burger");
-INSERT INTO burgers (burger_name, devoured) VALUE ("Cheese Burger", true );
-INSERT INTO burgers (burger_name, devoured) VALUE ("Kids Burger", true );
+INSERT INTO burger (burger_name) VALUE ("Double Double Burger");
+INSERT INTO burger (burger_name) VALUE ("4 X Burger");
+INSERT INTO burger (burger_name, devoured) VALUE ("Cheese Burger", true );
+INSERT INTO burger (burger_name, devoured) VALUE ("Kids Burger", true );
