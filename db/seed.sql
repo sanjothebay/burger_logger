76c5821e150c8cbdb228ffb950ae44a7fcb7f35f
JC <sanjothebay@gmail.com>
2021-01-09 23:10:42 -0600

    added my sql files seed and schema

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUE ("Double Double Burger");
+INSERT INTO burgers (burger_name) VALUE ("4 X Burger");
+INSERT INTO burgers (burger_name, devoured) VALUE ("Cheese Burger", true );
+INSERT INTO burgers (burger_name, devoured) VALUE ("Kids Burger", true );
